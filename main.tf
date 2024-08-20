30e2284f1ebfa827475f4b49fdc2bfc4ecb06716
Susmitha2601 <130479675+Susmitha2601@users.noreply.github.com>
2024-08-20 23:30:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,42 +3,42 @@ provider "aws" {
 }
 
 # VPC
-resource "aws_vpc" "main6" {
+resource "aws_vpc" "main7" {
   cidr_block = "10.0.0.0/16"
 }
 
 # Subnets
-resource "aws_subnet" "public6" {
+resource "aws_subnet" "public7" {
   count                   = 2
-  vpc_id                  = aws_vpc.main6.id
-  cidr_block              = cidrsubnet(aws_vpc.main6.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.main7.id
+  cidr_block              = cidrsubnet(aws_vpc.main7.cidr_block, 8, count.index)
   availability_zone       = element(["us-west-2a", "us-west-2b"], count.index)
   map_public_ip_on_launch = true
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "igw6" {
-  vpc_id = aws_vpc.main6.id
+resource "aws_internet_gateway" "igw7" {
+  vpc_id = aws_vpc.main7.id
 }
 
 # Route Table
-resource "aws_route_table" "public6" {
-  vpc_id = aws_vpc.main6.id
+resource "aws_route_table" "public7" {
+  vpc_id = aws_vpc.main7.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw6.id
+    gateway_id = aws_internet_gateway.igw7.id
   }
 }
 
-resource "aws_route_table_association" "public6" {
+resource "aws_route_table_association" "public7" {
   count          = 2
-  subnet_id      = element(aws_subnet.public6.*.id, count.index)
-  route_table_id = aws_route_table.public6.id
+  subnet_id      = element(aws_subnet.public7.*.id, count.index)
+  route_table_id = aws_route_table.public7.id
 }
 
 # Security Group
-resource "aws_security_group" "ecs_sg6" {
-  vpc_id = aws_vpc.main6.id
+resource "aws_security_group" "ecs_sg7" {
+  vpc_id = aws_vpc.main7.id
 
   ingress {
     from_port   = 80
@@ -56,8 +56,8 @@ resource "aws_security_group" "ecs_sg6" {
 }
 
 # ECS Task Execution Role
-resource "aws_iam_role" "ecs_task_execution_role6" {
-  name = "ecsTaskExecutionRole2"
+resource "aws_iam_role" "ecs_task_execution_role7" {
+  name = "ecsTaskExecutionRole3"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -73,65 +73,65 @@ resource "aws_iam_role" "ecs_task_execution_role6" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy6" {
-  role       = aws_iam_role.ecs_task_execution_role6.name
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy7" {
+  role       = aws_iam_role.ecs_task_execution_role7.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # ECR Repository
-resource "aws_ecr_repository" "jenkins6" {
-  name = "jenkins2"
+resource "aws_ecr_repository" "jenkins7" {
+  name = "jenkins3"
 }
 
 # ECS Cluster
-resource "aws_ecs_cluster" "jenkins_cluster6" {
-  name = "jenkins-cluster1"
+resource "aws_ecs_cluster" "jenkins_cluster7" {
+  name = "jenkins-cluster3"
 }
 
 # ALB
-resource "aws_lb" "jenkins_alb6" {
-  name               = "jenkins-alb1"
+resource "aws_lb" "jenkins_alb7" {
+  name               = "jenkins-alb3"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.ecs_sg6.id]
-  subnets            = aws_subnet.public6.*.id
+  security_groups    = [aws_security_group.ecs_sg7.id]
+  subnets            = aws_subnet.public7.*.id
 }
 
 # ALB Target Group
-resource "aws_lb_target_group" "jenkins_tg6" {
-  name     = "jenkins-tg2"
+resource "aws_lb_target_group" "jenkins_tg7" {
+  name     = "jenkins-tg3"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main6.id
+  vpc_id   = aws_vpc.main7.id
 
 target_type = "ip"
 }
 
 # ALB Listener
-resource "aws_lb_listener" "jenkins_listener6" {
-  load_balancer_arn = aws_lb.jenkins_alb6.arn
+resource "aws_lb_listener" "jenkins_listener7" {
+  load_balancer_arn = aws_lb.jenkins_alb7.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.jenkins_tg6.arn
+    target_group_arn = aws_lb_target_group.jenkins_tg7.arn
   }
 }
 
 # ECS Task Definition
-resource "aws_ecs_task_definition" "jenkins_task6" {
-  family                   = "jenkins-task1"
+resource "aws_ecs_task_definition" "jenkins_task7" {
+  family                   = "jenkins-task3"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role6.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role7.arn
   memory                   = "512"
-  cpu                      = "256"
+  cpu                      = "257"
 
   container_definitions = jsonencode([
     {
-      name      = "jenkins2"
-      image     = "${aws_ecr_repository.jenkins6.repository_url}:latest"
+      name      = "jenkins3"
+      image     = "${aws_ecr_repository.jenkins7.repository_url}:latest"
       essential = true
       portMappings = [
         {
@@ -144,20 +144,20 @@ resource "aws_ecs_task_definition" "jenkins_task6" {
 }
 
 # ECS Service
-resource "aws_ecs_service" "jenkins_service6" {
-  name            = "jenkins-service1"
-  cluster         = aws_ecs_cluster.jenkins_cluster6.id
-  task_definition = aws_ecs_task_definition.jenkins_task6.arn
+resource "aws_ecs_service" "jenkins_service7" {
+  name            = "jenkins-service3"
+  cluster         = aws_ecs_cluster.jenkins_cluster7.id
+  task_definition = aws_ecs_task_definition.jenkins_task7.arn
   launch_type     = "FARGATE"
   desired_count   = 1
   network_configuration {
-    subnets         = aws_subnet.public6.*.id
-    security_groups = [aws_security_group.ecs_sg6.id]
+    subnets         = aws_subnet.public7.*.id
+    security_groups = [aws_security_group.ecs_sg7.id]
   }
   load_balancer {
-    target_group_arn = aws_lb_target_group.jenkins_tg6.arn
-    container_name   = "jenkins1"
+    target_group_arn = aws_lb_target_group.jenkins_tg7.arn
+    container_name   = "jenkins3"
     container_port   = 8080
   }
-  depends_on = [aws_lb_listener.jenkins_listener6]
+  depends_on = [aws_lb_listener.jenkins_listener7]
 }
